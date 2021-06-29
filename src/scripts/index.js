7efddb439fb4432753bb3dd113d867fb72f74b89
André <andreglock@gmail.com>
2021-06-29 13:50:51 +0200

    done?

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,4 +4,32 @@ import "babel-polyfill";
 
 
 // \/ All of your javascript should go here \/
+const form = document.querySelector("#submit-form");
+const name = document.querySelector("#name");
+const email = document.querySelector("#email");
+const message = document.querySelector("#message");
+const checkbox = document.querySelector("#checkbox");
 
+const getUser = async (user) => {
+    try {
+        const data = await fetch("https://jsonplaceholder.typicode.com/users", {
+            method: "POST",
+            body: JSON.stringify(user),
+        });
+        return data;
+    } catch {
+        console.warn("Error");
+    }
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault(); // prevents the form to be send
+    alert("You submitted");
+    const user = {
+        name: name.value,
+        email: email.value,
+        message: message.value,
+        checkbox: checkbox.checked,
+    };
+    getUser(user).then(console.log);
+});
